60663e1d54b267cee9c7e6111698b26f0f903bf5
Bernardo Munz <moonbe77@gmail.com>
2021-01-10 13:42:26 +1100

    FIX: white broders on body

--- components/layout/MainLayout.js
+++ components/layout/MainLayout.js
@@ -8,12 +8,14 @@ export default function Layout({ children }) {
   const theme = isDarkTheme ? style.dark : style.light;
 
   return (
-    <div className={`${style.container} ${theme}`}>
-      <Header />
-      <main className={style.content}>{children}</main>
-      <footer className={style.footer}>
-        Copyright 2020 - Built with Next.js
-      </footer>
+    <div className={`${style.bodyWrapper} ${theme}`}>
+      <div className={`${style.container}`}>
+        <Header />
+        <main className={style.content}>{children}</main>
+        <footer className={style.footer}>
+          Copyright 2020 - Built with Next.js
+        </footer>
+      </div>
     </div>
   );
 }
