f13e5d044f82fd5acffb0a5de3ed0799b805e33c
Bernardo <moonbe77@gmail.com>
2021-01-30 20:31:46 +1100

    add getStaticProps for projects data

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,22 +1,34 @@
 import Head from 'next/head';
+import projects from '../public/data/projects.json';
 import { useStateContext } from '../store/store';
 import PortfolioProject from '../components/molecules/PortfolioProject';
 
 // import 'aos/dist/aos.css';
-const Portfolio = () => {
+const Portfolio = ({projects}) => {
   const state = useStateContext();
-  const { projects, isDarkTheme } = state;
+  const { isDarkTheme } = state;
 
   return (
     <>
       <Head>
         <title>Portfolio</title>
       </Head>
-      {projects.map((project, i) => (
-        <PortfolioProject key={i} project={project} isDarkTheme={isDarkTheme} />
-      ))}
+      {projects.data &&
+        projects.data.map((project, i) => (
+          <PortfolioProject
+            key={i}
+            project={project}
+            isDarkTheme={isDarkTheme}
+          />
+        ))}
     </>
   );
 };
 
 export default Portfolio;
+
+export async function getStaticProps() {
+  return {
+    props: { projects }, // will be passed to the page component as props
+  };
+}
