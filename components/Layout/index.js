35565dac261e02cf3aef7f121d60415d2b8f60c1
Bernardo <moonbe77@gmail.com>
2021-03-30 21:12:49 +1100

    FIX: tictactoe background full width

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -48,82 +48,81 @@ export default function Layout({ children }) {
   }, [setPlaying]);
 
   return (
-    <animated.div style={fade} className={`${theme}`}>
-      <div className={`${style.container}`}>
-        <Header />
-        <main className={style.content}>{children}</main>
-        {transitions.map(
-          ({ item, key, props }) =>
-            item && (
-              <animated.div
-                className={style.gameWrapper}
-                key={key}
-                style={props}
+    <>
+      <animated.div style={fade} className={`${theme}`}>
+        <div className={`${style.container}`}>
+          <Header />
+          <main className={style.content}>{children}</main>
+
+          <footer className={style.footer}>
+            <div className={style.contact}>
+              <div
+                className="LI-profile-badge"
+                data-version="v1"
+                data-size="medium"
+                data-locale="es_ES"
+                data-type="horizontal"
+                data-theme="dark"
+                data-vanity="munzbe"
               >
-                <TicTacToe />
-              </animated.div>
-            )
-        )}
-        <footer className={style.footer}>
-          <div className={style.contact}>
-            <div
-              className="LI-profile-badge"
-              data-version="v1"
-              data-size="medium"
-              data-locale="es_ES"
-              data-type="horizontal"
-              data-theme="dark"
-              data-vanity="munzbe"
-            >
-              <a
-                className="LI-simple-link"
-                href="https://au.linkedin.com/in/munzbe?trk=profile-badge"
-              >
-                Bernardo Munz
-              </a>
+                <a
+                  className="LI-simple-link"
+                  href="https://au.linkedin.com/in/munzbe?trk=profile-badge"
+                >
+                  Bernardo Munz
+                </a>
+              </div>
+              <div>2021</div>
             </div>
-            <div>2021</div>
-          </div>
-          <div className={style.spotify}>
-            <div className={style.spotifyIcon}>
-              <FaSpotify />
-            </div>
-            <div className={style.spotifyPlayingNow}>
-              <h4>Playing now:</h4>
+            <div className={style.spotify}>
+              <div className={style.spotifyIcon}>
+                <FaSpotify />
+              </div>
+              <div className={style.spotifyPlayingNow}>
+                <h4>Playing now:</h4>
+                <div>
+                  <FaPlayCircle /> {playing?.title} - {playing?.album} -{' '}
+                  {playing?.artist}
+                </div>
+              </div>
               <div>
-                <FaPlayCircle /> {playing?.title} - {playing?.album} -{' '}
-                {playing?.artist}
+                <h4>My top 10 Songs Listened</h4>
+                <ul className={style.spotifyList}>
+                  {lastSong &&
+                    lastSong.tracks.map((song, i) => (
+                      <li key={i}>
+                        <a
+                          href={song.songUrl}
+                          target="_blank"
+                          rel="noreferrer"
+                          alt={`link to ${song.title}`}
+                        >{`${song.title} - ${song.artist}`}</a>
+                      </li>
+                    ))}
+                </ul>
               </div>
             </div>
-            <div>
-              <h4>My top 10 Songs Listened</h4>
-              <ul className={style.spotifyList}>
-                {lastSong &&
-                  lastSong.tracks.map((song, i) => (
-                    <li key={i}>
-                      <a
-                        href={song.songUrl}
-                        target="_blank"
-                        rel="noreferrer"
-                        alt={`link to ${song.title}`}
-                      >{`${song.title} - ${song.artist}`}</a>
-                    </li>
-                  ))}
-              </ul>
-            </div>
+          </footer>
+          <div className={`${style.elipse} ${style.elipse1}`}>
+            <img src="/figma/elipses/Ellipse1.svg" alt="" srcSet="" />
           </div>
-        </footer>
-        <div className={`${style.elipse} ${style.elipse1}`}>
-          <img src="/figma/elipses/Ellipse1.svg" alt="" srcSet="" />
+          <div className={`${style.elipse} ${style.elipse2}`}>
+            <img src="/figma/elipses/Ellipse2.svg" alt="" srcSet="" />
+          </div>
+          <div className={`${style.elipse} ${style.elipse3}`}>
+            <img src="/figma/elipses/Ellipse3.svg" alt="" srcSet="" />
+          </div>
         </div>
-        <div className={`${style.elipse} ${style.elipse2}`}>
-          <img src="/figma/elipses/Ellipse2.svg" alt="" srcSet="" />
-        </div>
-        <div className={`${style.elipse} ${style.elipse3}`}>
-          <img src="/figma/elipses/Ellipse3.svg" alt="" srcSet="" />
-        </div>
-      </div>
-    </animated.div>
+      </animated.div>
+      {transitions.map(
+        ({ item, key, props }) =>
+          item && (
+            <animated.div className={style.gameWrapper} key={key} style={props}>
+              <TicTacToe />
+            </animated.div>
+          )
+      )}
+    </>
   );
 }
 
