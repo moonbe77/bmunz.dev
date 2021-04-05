83833d884ec70d77b20b4d751ebd53b9ad7d50d8
Bernardo <moonbe77@gmail.com>
2021-04-05 14:33:50 +1000

    adding context to storybook

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -3,13 +3,20 @@ import Button from '../components/atoms/Button';
 
 export default {
   title: 'Design System/Atoms/Button',
+
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
     isDarkTheme: { control: 'boolean' },
   },
 };
+
 const Template = (args) => <Button {...args} />;
+Template.parameters = {
+  initialState: {
+    isDarkTheme: true,
+  },
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
