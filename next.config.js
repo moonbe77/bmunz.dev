99326b1b473cf11aa05e16c4ba1544b01223e1c8
Bernardo <moonbe77@gmail.com>
2021-04-17 18:29:14 +1000

    added serviceworker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
   env: {},
   future: {
     webpack5: true,
   },
-};
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+});
