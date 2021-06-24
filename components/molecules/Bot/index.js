a0cf79685bb895ade18b30f3f99909912a5c6af2
Bernardo <moonbe77@gmail.com>
2021-06-24 20:17:49 +1000

    add suggestion, multiple answers from bot

--- components/molecules/Bot/index.js
+++ components/molecules/Bot/index.js
@@ -1,44 +1,92 @@
 import { useState, useEffect, useRef } from 'react';
 import { askToBot } from '../../../utils/askToBot';
 import styles from './bot.module.scss';
+import Button from '../../atoms/Button';
 
-const MyMessage = (message) => <li className={styles.myMessage}>{message}</li>;
+const TextMessage = (message) => (
+  <li className={styles.myMessage}>{message}</li>
+);
+
+const Suggestions = ({ data, action }) => {
+  if (!data) return 'loading..';
+
+  return (
+    <div className={styles.suggestions}>
+      {data.map((slug, i) => (
+        <Button key={i} onClick={action} role="button">
+          {slug}
+        </Button>
+      ))}
+    </div>
+  );
+};
 
 const Bot = () => {
   const [messages, setMessages] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  // const [ask, setAsk] = useState('hello');
+  const [suggestions, setSuggestions] = useState(null);
+  const [isWaitingAnswer, setIsWaitingAnswer] = useState(false);
   const messagesContainer = useRef();
+  const isWaiting = isWaitingAnswer ? styles.waiting : '';
+
+  const messageParser = (message, origin) => {
+    // pareced of the message to be added in the chat
+    console.log(message);
+    if (message.fulfillmentMessages) {
+      message.fulfillmentMessages.map((type) => {
+        if (type.message === 'text') {
+          type.text.text.map((txt) => {
+            setMessages((prev) => [
+              ...prev,
+              {
+                id: Date.now(),
+                text: txt,
+                sender: 'bot',
+              },
+            ]);
+          });
+          return;
+        }
+
+        if (type.message === 'card') {
+          console.log(type);
+        }
+
+        if (type.message === 'quickReplies') {
+          console.log(type);
+          setSuggestions(type.quickReplies.quickReplies);
+        }
+      });
+    } else {
+      setMessages((prev) => [
+        ...prev,
+        {
+          id: Date.now(),
+          text: typeof message === 'object' ? message.fulfillmentText : message,
+          sender: origin,
+        },
+      ]);
+    }
+  };
 
   useEffect(() => {
     messagesContainer.current.scrollTop =
       messagesContainer.current.scrollHeight + 35;
   }, [messages]);
 
-  const addMessage = (message, origin) => {
-    console.log(message);
-    if (message.fulfillmentMessages) {
-      message.fulfillmentMessages.map((type) => {
-        if (type.message === 'text') {
-          console.log(type);
-        }
-        if (type.message === 'card') {
-          console.log(type);
-        }
-        if (type.message === 'quickReplies') {
-          console.log(type);
-        }
-      });
-    }
+  function askToBotHandler(query) {
+    askToBot(query).then((data) => {
+      setIsWaitingAnswer(false);
+      messageParser(data, 'bot');
+    });
+  }
 
-    setMessages((prev) => [
-      ...prev,
-      {
-        id: Date.now(),
-        text: typeof message === 'object' ? message.fulfillmentText : message,
-        sender: origin,
-      },
-    ]);
-  };
+  function addUserQueryToChat(message) {
+    setIsWaitingAnswer(true);
+    messageParser(message, origin);
+    askToBotHandler(message);
+  }
 
   const handleInput = (e) => {
     setInputValue(e.target.value);
@@ -46,21 +94,21 @@ const Bot = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (inputValue) {
-      addMessage(inputValue);
-      setInputValue('');
-
-      // useAskToBot returns a promise
-      askToBot(inputValue).then((data) => {
-        addMessage(data, 'bot');
-      });
+      addUserQueryToChat(inputValue, 'user');
     }
+    setInputValue('');
+  };
+
+  const handleClickSuggestion = (e) => {
+    setIsWaitingAnswer(true);
+    const value = e.target.innerText.toLowerCase();
+    addUserQueryToChat(value, 'user');
   };
 
   return (
     <div className={styles.botContainer}>
-      <h1>BotMunz</h1>
+      <h1>Munz_Bot</h1>
       <ul className={styles.messagesContainer} ref={messagesContainer}>
         {messages &&
           messages.map((message) => (
@@ -77,15 +125,22 @@ const Bot = () => {
           ))}
         {/* <li style={{ float: 'left', clear: 'both' }}  /> */}
       </ul>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <input
-          name="queryValue"
-          type="text"
-          onChange={handleInput}
-          value={inputValue}
-        />
-        <button>submit</button>
-      </form>
+      <div className={`${styles.formWrapper} ${isWaiting}`}>
+        <div>
+          {suggestions && (
+            <Suggestions data={suggestions} action={handleClickSuggestion} />
+          )}
+        </div>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <input
+            name="queryValue"
+            type="text"
+            onChange={handleInput}
+            value={inputValue}
+          />
+          <button>submit</button>
+        </form>
+      </div>
     </div>
   );
 };
