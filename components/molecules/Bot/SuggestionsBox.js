8927b1de1db3599cbd870137f0199de98c767b12
Bernardo <moonbe77@gmail.com>
2021-07-11 19:47:09 +1000

    FIX

--- components/molecules/Bot/SuggestionsBox.js
+++ components/molecules/Bot/SuggestionsBox.js
@@ -1,25 +1,22 @@
 import PropTypes from 'prop-types';
 import styles from './suggestionsBox.module.scss';
 
-const SuggestionsBox = ({ data, action }) => {
-  if (!data) return 'loading..';
+const SuggestionsBox = ({ data, action }) => (
+  // if (!data) return 'loading..';
 
-  return (
-    <div className={styles.suggestions}>
-      {data.map((slug, i) => (
-        <div
-          className={styles.suggestionTag}
-          key={i}
-          onClick={action}
-          role="button"
-        >
-          {slug}
-        </div>
-      ))}
-    </div>
-  );
-};
-
+  <div className={styles.suggestions}>
+    {data.map((slug, i) => (
+      <div
+        className={styles.suggestionTag}
+        key={i}
+        onClick={action}
+        role="button"
+      >
+        {slug}
+      </div>
+    ))}
+  </div>
+);
 export default SuggestionsBox;
 
 SuggestionsBox.propTypes = {
