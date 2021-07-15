a45df414a317f44020dc6c28ecaf0359861d21f9
Bernardo <moonbe77@gmail.com>
2021-07-15 22:09:56 +1000

    chore;

--- components/molecules/PortfolioProjectWrapper/index.js
+++ components/molecules/PortfolioProjectWrapper/index.js
@@ -3,42 +3,39 @@ import PropTypes from 'prop-types';
 import Project from '../Project';
 import style from './portfolioProjectsWrapper.module.scss';
 
-const PortfolioProjectsWrapper = ({ projects, isDarkTheme }) => {
-  const container = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.5,
-      },
-    },
-  };
+const PortfolioProjectsWrapper = ({ projects, isDarkTheme }) => (
+  // const container = {
+  //   hidden: { opacity: 0 },
+  //   show: {
+  //     opacity: 1,
+  //     transition: {
+  //       staggerChildren: 0.5,
+  //     },
+  //   },
+  // };
 
-  const item = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1 },
-  };
-  return (
-    <motion.section className={style.projectsWrapper}>
-      <AnimatePresence>
-        <h1>What I've Been Doing</h1>
-        {/* <div>
+  // const item = {
+  //   hidden: { opacity: 0 },
+  //   show: { opacity: 1 },
+  // };
+  <motion.section className={style.projectsWrapper}>
+    <AnimatePresence>
+      <h1>What I've Been Doing</h1>
+      {/* <div>
           click on the icon to filter <span>🎈🧨🎗🎟🎏</span>
         </div> */}
-        <div>
-          {projects.map((project, i) => (
-            <Project
-              key={project.id}
-              project={project}
-              isDarkTheme={isDarkTheme}
-            />
-          ))}
-        </div>
-      </AnimatePresence>
-    </motion.section>
-  );
-};
-
+      <div>
+        {projects.map((project) => (
+          <Project
+            key={project.id}
+            project={project}
+            isDarkTheme={isDarkTheme}
+          />
+        ))}
+      </div>
+    </AnimatePresence>
+  </motion.section>
+);
 export default PortfolioProjectsWrapper;
 
 PortfolioProjectsWrapper.propTypes = {
